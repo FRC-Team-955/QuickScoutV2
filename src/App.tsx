8c22739f150f92758c13b7ea39e1da78adb58c39
PioneerSwine996 <pioneerswine9@gmail.com>
2026-02-03 19:04:37 -0800

    Maybe remove 404

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <AuthProvider>
-        <BrowserRouter basename="/QuickScoutV2/">
+        <BrowserRouter basename="/QuickScoutV2/login">
           <Routes>
             <Route
               path="/login"
